7d8f281df9b30a1407db4b7f71e5c9c40a148eda
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-16 07:15:15 +0300

    sm8250-common: add bt_stack_qti.conf to soong namespaces
    bt_stack_config: init attempt to load stack conf from /etc/bluetooth/bt_stack_qti.conf
    bt_osi_config: config_new unable to open file '/etc/bluetooth/bt_stack_qti.conf': No such file or directory
    bt_stack_config: init file >/etc/bluetooth/bt_stack_qti.conf< not found

--- common.mk
+++ common.mk
@@ -430,7 +430,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    device/oneplus/common
+    device/oneplus/common \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
