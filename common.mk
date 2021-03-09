4bcd5c046a2d9493795f31ec5066f66821e51b15
apophis9283 <steaven.android@gmail.com>
2021-03-09 14:06:23 -0500

    Missing stuff

--- common.mk
+++ common.mk
@@ -212,6 +212,7 @@ PRODUCT_PACKAGES += \
     ftm_power_config.sh \
     init.class_main.sh \
     init.crda.sh \
+    init.opcamera.rc \
     init.cust.rc \
     init.mdm.sh \
     init.oem.debug.rc \
