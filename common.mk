444e6f43c5c809dfb62f922fcec2a634e826e6c8
apophis9283 <steaven.android@gmail.com>
2021-03-19 23:34:16 -0400

    Revert "sm8250-common: update Hotword Enrollment blobs and whitelisted"
    This reverts commit 788e2794cff299cb074c5628c292ff05aadcc9ac.

--- common.mk
+++ common.mk
@@ -338,7 +338,6 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hotwordenrollment-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotwordenrollment-hiddenapi-package-allowlist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Input
